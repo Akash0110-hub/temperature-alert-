fd3d615c87f7583ac88e74cf755cfca76ace0db7
Akash0110-hub <akashgshettar10@gmail.com>
2025-10-04 09:25:18 +0530

    Update Simple_interest.py

--- Simple_interest.py
+++ Simple_interest.py
@@ -5,5 +5,8 @@ P = float(input("Enter the Principal amount: "))
 R = float(input("Enter the Rate of Interest: "))
 T = float(input("Enter the Time (in years): "))
 
+#calculate the interset
+SI = (P*T*R)/100
+
 # Display result
 print("The Simple Interest is:", SI)
